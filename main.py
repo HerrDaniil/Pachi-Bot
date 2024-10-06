59a99c81a41d1f80c790ce862b4c0b9d42046426
Носач Даниіл bc2024-2 <nosacdaniil@gmail.com>
2024-10-07 00:48:44 +0300

    Добавил сообщение при заходе и выходе с сервера. Добавлено распознавание 'you amazing' и рандомных ответов на это.

--- main.py
+++ main.py
@@ -1,17 +1,56 @@
 import discord
 from discord.ext import commands
+from tokens import *
+import random
 
 intents = discord.Intents.default()
+intents.members = True
 intents.message_content = True
 
-client = commands.Bot(command_prefix = '!', intents=intents)
+client = commands.Bot(command_prefix='!', intents=intents)
 
 @client.event
 async def on_ready():
-    print("My name is Kira Yoshikage and i'm ready to start working")
+    print("My name is Kira Yoshikage and I'm ready to start working")
+
+@client.event
+async def on_member_join(member):
+
+    channel = client.get_channel(1292233022640361548)
+
+    responses = [
+        f"{member.display_name}, you amazing!",
+        f"Это же {member.display_name}! Сколько же мы его ждали!",
+        f"Это же наш любимый {member.display_name}!",
+    ]
+
+    response = random.choice(responses)
+        
+    await channel.send(response)
+
+@client.event
+async def on_member_remove(member):
+    channel = client.get_channel(1292233022640361548)
+    await channel.send("SKIBIDI SIGMA, {member.display_name}, LEFT THE SERVER NOOOOOOOOOOO")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if 'you amazing' in message.content.lower():
+        responses = [
+            f"Нет, {message.author.display_name}, you amazing!",
+            f"Спасибо за комплимент, но хочу сказать, что you amazing!",
+            f"{message.author.display_name}, я конечно amazing, но ты просто amazing",
+        ]
+
+        response = random.choice(responses)
+        
+        await message.channel.send(response)
 
 @client.command()
 async def hello(ctx):
     await ctx.send("Hello, Dexter Morgan")
 
-client.run('MTI5MjI3MjYyNTg1ODY0NjA3Ng.Ggi6-E.uIOx_BAskw9QMf19a-U7qE-mwGBdXVbdXUBOd8')
+client.run(ToshaToken)
